9df27968bc26cbfb9dc6be6a677e4284f1bb25e8
Thomas <thomasmercierlecorre615@gmail.com>
2026-05-27 21:12:03 +0200

    Version 1.3

--- Assets/Script/MenuCrontroller.cs
+++ Assets/Script/MenuCrontroller.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
-public class MenuCrontroller : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null; //corriger avec IA j'ai mis TMP_text au lieu de TextMeshProUGUI
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private GameObject confirmationPromt = null;
+
+
+
     [Header("Levels To Load")]
     public string newgamelvl;
     private string lvlToload;
@@ -26,9 +36,28 @@ public class MenuCrontroller : MonoBehaviour
             noSavedGameDialog.SetActive(true);
         }
     }
-
     public void ExitButton()
     {
         Application.Quit();
     }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPromt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPromt.SetActive(false);
+    }
+
 }
